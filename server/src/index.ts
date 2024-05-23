38988bb324f17d21784d4e04f06cf1b866aaf244
nabeel-ncz <nabeelnabhan115@gmail.com>
2024-05-23 17:35:13 +0530

    initial: server confiured using express with one test endpoint

---
+++ server/src/index.ts
@@ -0,0 +1,53 @@
+import express, { Application, Request, Response } from "express";
+const app: Application = express();
+
+
+app.all('/health', (req: Request, res: Response) => {
+    res.send("⚡⚡⚡");
+})
+
+
+app.get('/api/dashboard', (req: Request, res: Response) => {
+    res.status(200).json({data: [
+        {
+            title: 'Component 1',
+            description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry.            '
+        },
+        {
+            title: 'Component 2',
+            description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry.            '
+        },
+        {
+            title: 'Component 3',
+            description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry.            '
+        },
+        {
+            title: 'Component 4',
+            description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry.            '
+        },
+        {
+            title: 'Component 5',
+            description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry.            '
+        },
+        {
+            title: 'Component 6',
+            description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry.            '
+        },
+        {
+            title: 'Component 7',
+            description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry.            '
+        },
+        {
+            title: 'Component 8',
+            description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry.            '
+        },
+        {
+            title: 'Component 9',
+            description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry.            '
+        },
+    ]})
+})
+
+app.listen(4000, () => {
+    console.log(`Server is listenting at ${4000}`);
+});
