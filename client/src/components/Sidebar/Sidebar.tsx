cf300ee6b83d2ddc257012cf9ce4cb7f8687b0c4
nabeel-ncz <nabeelnabhan115@gmail.com>
2024-05-23 18:01:21 +0530

    feat: fetching dashboard data from backend

--- client/src/components/Sidebar/Sidebar.tsx
+++ client/src/components/Sidebar/Sidebar.tsx
@@ -16,7 +16,7 @@ export default function Sidebar({
 }) {
 
     return (
-        <div className={`w-full h-screen sticky bg-[#000] pt-4 pl-4 xl:block ${open ? 'block z-10 bg-[rgba(0,0,0,0.7)] backdrop-blur-lg px-6' : 'hidden'}`}>
+        <div className={`w-full h-screen sticky bg-[#000] pt-4 pl-4 transition-all ease-in-out xl:block ${open ? 'block z-10 bg-[rgba(0,0,0,0.7)] backdrop-blur-lg px-6' : 'hidden'}`}>
             <div className="flex items-center justify-between gap-4">
                 <img src="/ginger-logo.png" alt="" className="h-14" />
                 {open && (
@@ -38,25 +38,25 @@ export default function Sidebar({
             <div className="flex flex-col items-start pt-8">
                 <h2 className="font-medium text-xs text-[rgba(255,255,255,0.6)] uppercase">Manage</h2>
                 <div className="flex flex-col items-start pt-4 gap-8">
-                    <NavLink to='/inbox' className={({ isActive }) => isActive ? "opacity-100" : "opacity-50"}>
+                    <NavLink to='/inbox' className={({ isActive }) => isActive ? "opacity-100" : "opacity-50 hover:opacity-100"}>
                         <div className="flex items-center justify-start gap-2 cursor-pointer">
                             <AiFillMessage size={"22"} color={"#fff"} />
                             <h2 className="font-normal text-sm text-white">Inbox</h2>
                         </div>
                     </NavLink>
-                    <NavLink to='/channels' className={({ isActive }) => isActive ? "opacity-100" : "opacity-50"}>
+                    <NavLink to='/channels' className={({ isActive }) => isActive ? "opacity-100" : "opacity-50 hover:opacity-100"}>
                         <div className="flex items-center justify-start gap-2 cursor-pointer">
                             <MdDashboard size={"22"} color={"#fff"} />
                             <h2 className="font-normal text-sm text-white">Channels</h2>
                         </div>
                     </NavLink>
-                    <NavLink to='/profiles' className={({ isActive }) => isActive ? "opacity-100" : "opacity-50"}>
+                    <NavLink to='/profiles' className={({ isActive }) => isActive ? "opacity-100" : "opacity-50 hover:opacity-100"}>
                         <div className="flex items-center justify-start gap-2 cursor-pointer">
                             <FaUser size={"22"} color={"#fff"} />
                             <h2 className="font-normal text-sm text-white">Buissness Profiles</h2>
                         </div>
                     </NavLink>
-                    <NavLink to='/insights' className={({ isActive }) => isActive ? "opacity-100" : "opacity-50"}>
+                    <NavLink to='/insights' className={({ isActive }) => isActive ? "opacity-100" : "opacity-50 hover:opacity-100"}>
                         <div className="flex items-center justify-start gap-2 cursor-pointer">
                             <CgInsights size={"22"} color={"#fff"} />
                             <h2 className="font-normal text-sm text-white">Insights</h2>
@@ -67,19 +67,19 @@ export default function Sidebar({
             <div className="flex flex-col items-start pt-8">
                 <h2 className="font-medium text-xs text-[rgba(255,255,255,0.6)] uppercase">Settings</h2>
                 <div className="flex flex-col items-start pt-4 gap-8">
-                    <NavLink to='/roles' className={({ isActive }) => isActive ? "opacity-100" : "opacity-50"}>
+                    <NavLink to='/roles' className={({ isActive }) => isActive ? "opacity-100" : "opacity-50 hover:opacity-100"}>
                         <div className="flex items-center justify-start gap-2 cursor-pointer">
                             <LuPlusCircle size={"22"} color={"#fff"} />
                             <h2 className="font-normal text-sm text-white">Create Roles</h2>
                         </div>
                     </NavLink>
-                    <NavLink to='/users' className={({ isActive }) => isActive ? "opacity-100" : "opacity-50"}>
+                    <NavLink to='/users' className={({ isActive }) => isActive ? "opacity-100" : "opacity-50 hover:opacity-100"}>
                         <div className="flex items-center justify-start gap-2 cursor-pointer">
                             <FaUserPlus size={"22"} color={"#fff"} />
                             <h2 className="font-normal text-sm text-white">Create Users</h2>
                         </div>
                     </NavLink>
-                    <NavLink to='/settings' className={({ isActive }) => isActive ? "opacity-100" : "opacity-50"}>
+                    <NavLink to='/settings' className={({ isActive }) => isActive ? "opacity-100" : "opacity-50 hover:opacity-100"}>
                         <div className="flex items-center justify-start gap-2 cursor-pointer">
                             <IoMdSettings size={"22"} color={"#fff"} />
                             <h2 className="font-normal text-sm text-white">Settings</h2>
--- client/src/pages/Dashboard.tsx
+++ client/src/pages/Dashboard.tsx
@@ -1,6 +1,26 @@
+import axios from "axios";
 import Card from "../components/Card/Card";
+import { useEffect, useState } from "react";
+import { BASE_URL } from "../constants";
 
 export default function Dashboard() {
+
+    const [loading, setLoading] = useState<boolean>(true);
+    const [data, setData] = useState<{ [key: string]: any }[] | null>(null);
+
+    useEffect(() => {
+        handleFetch();
+    }, []);
+
+    const handleFetch = () => {
+        axios.get(`${BASE_URL}/api/dashboard`).then((res) => {
+            console.log(res.data);
+            setData(res.data?.data);
+        }).finally(() => {
+            setLoading(false);
+        })
+    }
+
     return (
         <>
             <div className="w-full min-h-[80vh] pt-10 flex flex-col gap-6">
@@ -8,46 +28,28 @@ export default function Dashboard() {
                     <h2 className="font-bold text-base text-white">Dashboard</h2>
                     <span className="font-light text-xs text-white opacity-60">Dashboard / List</span>
                 </div>
-                <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-2 lg:gap-6 px-6 lg:pl-24 lg:pr-14">
-                    <Card
-                        title="Components"
-                        description="Lorem Ipsum is simply dummy text of the printing and typesetting industry. "
-                    />
-                    <Card
-                        title="Components"
-                        description="Lorem Ipsum is simply dummy text of the printing and typesetting industry. "
-                    />
-                    <Card
-                        title="Components"
-                        description="Lorem Ipsum is simply dummy text of the printing and typesetting industry. "
-                    />
-                
-                    <Card
-                        title="Components"
-                        description="Lorem Ipsum is simply dummy text of the printing and typesetting industry. "
-                    />
-                    <Card
-                        title="Components"
-                        description="Lorem Ipsum is simply dummy text of the printing and typesetting industry. "
-                    />
-                    <Card
-                        title="Components"
-                        description="Lorem Ipsum is simply dummy text of the printing and typesetting industry. "
-                    />
-                
-                    <Card
-                        title="Components"
-                        description="Lorem Ipsum is simply dummy text of the printing and typesetting industry. "
-                    />
-                    <Card
-                        title="Components"
-                        description="Lorem Ipsum is simply dummy text of the printing and typesetting industry. "
-                    />
-                    <Card
-                        title="Components"
-                        description="Lorem Ipsum is simply dummy text of the printing and typesetting industry. "
-                    />
-                </div>
+                {loading ? (
+                    <>
+                        <h2 className="font-semibold text-white text-lg">Loading...</h2>
+                    </>
+                ) : (
+                    <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-2 lg:gap-6 px-6 lg:pl-24 lg:pr-14">
+                        {(!data || data.length === 0) ? (
+                            <>
+                                <h2 className="font-semibold text-white text-lg">No data found!</h2>
+                            </>
+                        ) : (
+                            <>
+                                {data?.map((item) => (
+                                    <Card
+                                        title={item.title}
+                                        description={item.description}
+                                    />
+                                ))}
+                            </>
+                        )}
+                    </div>
+                )}
             </div>
         </>
     )
